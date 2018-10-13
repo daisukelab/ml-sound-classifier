2397c73518d7b30da6c54c28411b9503c3f2bc03
daisukelab <niz.edge@gmail.com>
2018-10-13 09:29:18 +0900

    Add is_training_enalbed in common.

--- common.py
+++ common.py
@@ -26,6 +26,8 @@ sys.path.insert(0, str(Path.cwd()))
 # # Configration
 def is_handling_audio(conf):
     return 'sampling_rate' in conf
+def is_training_enalbed(conf):
+    return conf.enable_training
 
 def test_conf(conf):
     if conf.model not in ['mobilenetv2', 'alexnet']:
@@ -72,6 +74,8 @@ def auto_complete_conf(conf):
         conf.eval_ensemble = True # Set False if samples_per_file > 1 but ensemble is not available
     if 'what_is_sample' not in conf:
         conf.what_is_sample = 'log mel-spectrogram'
+    if 'enable_training' not in conf:
+        conf.enable_training = True
 
 from config import *
 auto_complete_conf(conf)
@@ -90,7 +94,7 @@ def load_npy(conf, filename):
     return np.load(conf.folder / filename)
 
 # # Model
-if is_handling_audio(conf):
+if is_training_enalbed(conf):
     from model import create_model, freeze_model_layers
 
 # # Audio Utilities
